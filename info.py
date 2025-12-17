e22f10c7e4f81c20874a240c2fda84646375536b
mohdabdullahm36-byte <mohdabdullahm36@gmail.com>
2025-12-18 00:00:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,13 +100,13 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001959173522')) #Verificat
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100')) #If Anyone Set Your Bot In Any Group And Set Shortner In That Group Then In This Channel The All Details Come
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/dreamxbotz")   # Tutorial link for verification
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/howtodowywgh/11")   # Tutorial link for verification
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/dreamxbotz")   # Second tutorial link for verification
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/dreamxbotz")   # Third tutorial link for verification
 
 # Verification (Must Fill All Veriables. Else You Got Error
 SHORTENER_API = environ.get("SHORTENER_API", "753dde84bc10538b5ea1e113de3d8e38b9aeb724") # Shortener API key
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "https://Shortxlinks.com/") # Shortener website
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "Shortxlinks.com") # Shortener website
 
 SHORTENER_API2 = environ.get("SHORTENER_API2", "a7ac9b3012c67d7491414cf272d82593c75f6cbb")  # Shortener API key for second website
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "omegalinks.in") # Shortener website for second website
